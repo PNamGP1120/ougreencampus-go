3df2531f4b925be66d8b4c39801915b353a1bfff
PNamGP1120 <phuongnam.it0212@gmail.com>
2025-12-17 09:36:45 +0700

    init backend structure

--- internal/utils/validator.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "regexp"
-
-func IsValidEmail(email string) bool {
-	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
-	return re.MatchString(email)
-}
